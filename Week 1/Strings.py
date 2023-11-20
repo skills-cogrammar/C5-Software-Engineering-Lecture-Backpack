511aec774aa4809bd3be687ea18da8d960921077
Liano <130678376+LianoHyperion@users.noreply.github.com>
2023-11-20 18:58:01 +0200

    Added Slides & Code File

---
+++ Week 1/Strings.py
@@ -0,0 +1,19 @@
+double_quote = "The quick brown fox jumps over the lazy dog"
+single_quote = 'A'
+
+# Assigning a string to a variable
+name = "Miles"
+surname = "Morales"
+
+# Assigning the result of two concatenated strings to a variable called 'full_name'
+full_name = name + surname
+print(full_name)
+
+full_name = name + " " + surname
+print(full_name)
+
+# BONUS ALERT!!! --> Using formatted-strings :)
+full_name = f"{name} {surname}"
+
+# Printing whatever is stored in the full_name variable to the terminal
+print(full_name)
