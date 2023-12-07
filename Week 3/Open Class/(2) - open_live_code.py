c80bce9c6d0cbdc390bfe8a2ad1690153024933a
Liano <130678376+LianoHyperion@users.noreply.github.com>
2023-12-07 10:33:23 +0200

    Added Open Session II Live code

---
+++ Week 3/Open Class/(2) - open_live_code.py
@@ -0,0 +1,69 @@
+# For inside of a while
+
+# Infinite Loop
+# while True:
+#     user_input = input("Enter any word: ")
+
+#     if user_input == "exit":
+#         break
+
+#     new_word = ""
+#     for i in range(len(user_input)):
+
+#         if user_input[i] == user_input[i].lower():
+#             new_word += user_input[i].upper()
+#         else:
+#             new_word += user_input[i].lower()
+
+#     print(new_word)
+
+
+# Join and Split :)
+
+name = ["Liano", "Naidoo"]
+list_element = name[0]
+char_of_string_element = list_element[0]
+
+# print(name[0][0])
+# print(type(True))
+
+
+# text = "123456"
+
+# text = " ".join(text)
+# # 1 2 3 4 5 6
+
+# csv = "Liano, Naidoo, @gmail, 082"
+# csv = csv.split(", ")
+
+# print(f"Hello {csv[0]} {csv[1]}!")
+
+
+
+
+# print("Before Splitting: ", text)
+# splitted = text.split()
+# print("After Splitting: ", splitted)
+
+# print(splitted)
+
+# for i in range(len(splitted)):
+#     splitted[i] = int(splitted[i])
+
+# print(splitted)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
