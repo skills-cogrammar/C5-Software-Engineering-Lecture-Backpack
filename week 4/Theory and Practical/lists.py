a71a6d86488ba08a1a8a61223790a7af74aefed5
ArmandleRouxx <armandlr@hyperiondev.com>
2023-12-14 20:29:04 +0200

    Added slides and examples for theory and practical on lists

---
+++ week 4/Theory and Practical/lists.py
@@ -0,0 +1,91 @@
+num_list = [1,2,3,4,5]
+word_list = ["Word1", "Word2", "Word3"]
+bool_list = [False, True, True, False]
+list_2d = [[1,2,3], [4,5,6], [7,8,9]]
+mixed_list = ["Hello", 13, 87.98, True]
+
+
+# num_list = [1,2,3,4,5]
+# new_num_list = num_list
+
+# new_num_list[2] = 200
+# print(num_list)
+
+
+# Indexing
+
+my_list = ["This", "is", "our", "list", "!"]
+
+# print(my_list[0])
+# print(my_list[3])
+# print(my_list[-1])
+# print(my_list[-3])
+
+# Slicing
+
+# print(my_list[2:4])
+# print(my_list[:3])
+# print(my_list[2:])
+# print(my_list[::2])
+
+# print(my_list[-3:-1])
+# print(my_list[-3:])
+# print(my_list[-3:-1])
+# print(my_list[::-1])
+
+
+# Item assignment
+
+# my_list[1] = "was"
+# print(my_list)
+
+# num_list = [1,2,3,4,5]
+# num_list[3] = 9
+# print(num_list)
+
+
+# # Adding items
+# my_list.append(":)")
+# print(my_list)
+
+# my_list.insert(2, "insert")
+# print(my_list)
+
+# my_list.extend(["This", "is", "another", "list", "."])
+# print(my_list)
+
+# my_list += ["This", "is", "another", "list", "."]
+# print(my_list)
+
+# Removing items
+# my_list.remove("list")
+# print(my_list)
+
+# my_list.pop(0)
+# print(my_list)
+
+# my_list.clear()
+# print(my_list)
+
+# # List manipulation
+# my_list.sort()
+# print(my_list)
+
+# my_list.reverse()
+# print(my_list)
+
+# num_list = [1,2,3,4,5]
+# print(len(num_list))
+# print(max(num_list))
+# print(min(num_list))
+
+# my_list = ["This", "is", "our", "list", "!"]
+# for word in my_list:
+#     print(word)
+
+# for i in range(len(my_list)):
+#     print(my_list[i])
+
+# for i, word in enumerate(my_list):
+#     print(i, word)
+
