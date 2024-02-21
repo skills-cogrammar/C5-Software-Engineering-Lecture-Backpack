bd647d4ca44080337715bc40e50ceb1c90502da6
ArmandleRouxx <armandlr@hyperiondev.com>
2024-02-21 12:40:10 +0200

    Added slides and examples for portfolio

---
+++ Week 12/4 - Portfolio/3 - Wednesday/game.py
@@ -0,0 +1,70 @@
+import os
+
+class Item:
+
+    def __init__(self, value) -> None:
+        self.value = value
+
+
+class Coin(Item):
+
+    def __init__(self):
+        super().__init__(50)
+
+
+class Gem(Item):
+
+    def __init__(self) -> None:
+        super().__init__(100)
+
+
+class Player:
+
+    def __init__(self) -> None:
+        self.score = 0
+        self.x = 0
+        self.y = 0
+
+    def pick_up(self, item):
+        self.score += item.value
+
+    def update_coordinates(self, x=None, y=None):
+        if x : self.x = x
+        if y : self.y = y
+
+    def get_player_coords(self):
+        return (self.x, self.y)
+    
+    def __str__(self):
+        return "P"
+
+class World:
+
+    def __init__(self):
+        self.map = [["." for _ in range(5)] for _ in range(5)]
+        self.player = None
+
+    def set_player(self, player):
+        self.player = player
+
+    def move_player(self, direction):
+        movements = {"w": (0, -1), "s": (0, 1), "a":(-1, 0), "d":(1,0)}
+        player_coords = self.player.get_player_coords()
+        self.map[player_coords[0]][player_coords[1]] = "."
+        player_coords = tuple(map(lambda x, y: 
+                             x+y, movements[direction], player_coords))
+        self.player.update_coordinates(player_coords[0], player_coords[1])
+        self.plot_object(self.player)
+        
+
+    def plot_object(self, obj):
+        self.map[obj.x][obj.y] = obj
+
+    def display_map(self):
+        os.system('cls')
+        for y in self.map:
+            for x in y:
+                print(x, end="")
+            print()
+
+
